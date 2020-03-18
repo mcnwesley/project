a95fb5367dd27e7ede085aafeaad8c48170da0a3
Maicon <mcnwesleandro@gmail.com>
2020-03-17 21:20:05 -0300

    Updated and Finished toolbar Layout

--- app/src/main/java/br/com/maicon/ioasys/ui/HomeFragment.kt
+++ app/src/main/java/br/com/maicon/ioasys/ui/HomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 
 import br.com.maicon.ioasys.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.search_bar.*
 
 
 class HomeFragment : Fragment() {
@@ -19,4 +22,24 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        searchView.setOnSearchClickListener {
+            imageViewIoasysHome.visibility = View.GONE
+            searchView.maxWidth = Integer.MAX_VALUE
+            textViewInformation.visibility = View.GONE
+        }
+
+        searchView.setOnCloseListener {
+            imageViewIoasysHome.visibility = View.VISIBLE
+            textViewInformation.visibility = View.VISIBLE
+            false
+        }
+
+    }
 }
