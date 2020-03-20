51053277e8ffb9c190812ee1ef522e21be7029fe
Maicon <mcnwesleandro@gmail.com>
2020-03-20 00:28:13 -0300

    Toolbar e details finished

---
+++ app/src/main/java/br/com/maicon/ioasys/ui/DetailsFragment.kt
@@ -0,0 +1,17 @@
+package br.com.maicon.ioasys.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import br.com.maicon.ioasys.R
+import br.com.maicon.ioasys.app.BaseFragment
+
+
+class DetailsFragment : BaseFragment() {
+
+
+
+}
