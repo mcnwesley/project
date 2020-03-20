079ffce11911be2d21a64a28a15694e5f73d59ee
Maicon <mcnwesleandro@gmail.com>
2020-03-19 22:44:25 -0300

    Recycler View implemented on home fragment

--- app/src/main/java/br/com/maicon/ioasys/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/maicon/ioasys/ui/home/HomeFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import androidx.core.view.isGone
 
 import br.com.maicon.ioasys.R
+import br.com.maicon.ioasys.app.BaseFragment
+import br.com.maicon.ioasys.ui.home.adapter.EnterpriseAdapter
+import br.com.maicon.ioasys.ui.home.adapter.OnItemClickListener
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.search_bar.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment(), OnItemClickListener<String> {
+    private lateinit var adapter: EnterpriseAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +30,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupView()
+        setupRecyclerView()
     }
 
     private fun setupView() {
@@ -33,13 +38,28 @@ class HomeFragment : Fragment() {
             imageViewIoasysHome.visibility = View.GONE
             searchView.maxWidth = Integer.MAX_VALUE
             textViewInformation.visibility = View.GONE
+            recyclerViewList.visibility = View.VISIBLE
         }
 
         searchView.setOnCloseListener {
             imageViewIoasysHome.visibility = View.VISIBLE
             textViewInformation.visibility = View.VISIBLE
+            recyclerViewList.visibility = View.GONE
             false
         }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = EnterpriseAdapter(
+            mutableListOf(
+                "Google", "Facebook", "Insta", "Limão", "Jiló", "Figo", "Banana", "Maça", "Pineapple"
+            ), this
+        )
+        recyclerViewList.adapter = adapter
+
+    }
+
+    override fun onItemClick(item: String, position: Int) {
 
     }
 }
